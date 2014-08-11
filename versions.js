92c26a69e9ba835c998be1008da4bcea7c2a9083
Jordan Harband <ljharb@gmail.com>
2014-08-11 01:36:07 -0700

    Fixing a style issue reported by `npm run lint`

--- versions.js
+++ versions.js
@@ -10,8 +10,11 @@ module.exports = function getVersions(module) {
 			npm.commands.view([module, 'versions'], true, function (err, info) {
 				if (err) { throw err; }
 				var keys = Object.keys(info);
-				if (keys.length === 1) { resolve(info[keys[0]].versions); }
-				else { reject('multiple keys: no idea what to do here'); }
+				if (keys.length === 1) {
+					resolve(info[keys[0]].versions);
+				} else {
+					reject('multiple keys: no idea what to do here');
+				}
 			});
 		});
 	});

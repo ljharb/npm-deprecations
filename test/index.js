f7d5ae14b911b8b52faa6f80a148498f9aa9f151
Jordan Harband <ljharb@gmail.com>
2015-09-18 22:40:38 +0900

    Make sure new `object.assign` and `forms` versions don't break tests (new deprecations will).

--- test/index.js
+++ test/index.js
@@ -6,6 +6,15 @@ require('./versions');
 var deprecations = require('../');
 var test = require('tape');
 
+var compact = function (obj) {
+	return Object.keys(obj).reduce(function (map, key) {
+		if (obj[key]) {
+			map[key] = obj[key];
+		}
+		return map;
+	}, {});
+};
+
 test('errors', function (t) {
 	t.throws(deprecations, new TypeError('at least 1 module name is required'));
 	t.throws(function () { deprecations(3); }, new TypeError('module names must all be strings'));
@@ -32,59 +41,31 @@ test('works', function (t) {
 		'0.6.0': 'Please update to the latest version to ensure the latest security fixes in "qs"',
 		'0.7.0': 'Please update to the latest version to ensure the latest security fixes in "qs"',
 		'0.8.1': 'Please update to the latest version to ensure the latest security fixes in "qs"',
-		'0.9.0': 'Please update to the latest version to ensure the latest security fixes in "qs"',
-		'0.9.1': undefined,
-		'0.9.2': undefined,
-		'0.9.3': undefined,
-		'0.9.4': undefined,
-		'0.9.5': undefined,
-		'0.9.6': undefined,
-		'0.10.0': undefined,
-		'1.0.0': undefined,
-		'1.1.0': undefined,
-		'1.1.1': undefined,
-		'1.1.3': undefined
+		'0.9.0': 'Please update to the latest version to ensure the latest security fixes in "qs"'
 	};
 
 	t.test('with one module', function (st) {
-		st.plan(1);
-		var promise = deprecations('forms');
-		promise.then(function (messages) {
-			st.deepEqual(messages, {
-				forms: formMessages
-			});
+		st.plan(2);
+		deprecations('forms').then(function (messages) {
+			st.deepEqual(Object.keys(messages), ['forms'], 'has just one key');
+			st.deepEqual(compact(messages.forms), formMessages, 'forms messages match');
 		}).done();
 	});
 
 	t.test('with multiple modules', function (st) {
-		st.plan(1);
-		var promise = deprecations('forms', 'object.assign');
-		promise.then(function (messages) {
-			st.deepEqual(messages, {
-				forms: formMessages,
-				'object.assign': {
-					'0.1.0': 'Please upgrade to the latest spec-compliant version',
-					'0.1.1': 'Please upgrade to the latest spec-compliant version',
-					'0.2.0': 'Please upgrade to the latest spec-compliant version',
-					'0.2.1': 'Please upgrade to the latest spec-compliant version',
-					'0.3.1': 'Object.assign should accept any value except null and undefined, and should coerce to an object',
-					'0.4.0': 'Please update to the latest version',
-					'0.4.2': 'v0.4.3 doesn\'t modify function arguments, since it deoptimizes v8',
-					'0.4.3': undefined,
-					'0.5.0': undefined,
-					'1.0.0': undefined,
-					'1.0.1': undefined,
-					'1.0.2': undefined,
-					'1.0.3': undefined,
-					'1.1.1': undefined,
-					'2.0.0': undefined,
-					'2.0.1': undefined,
-					'2.0.2': undefined,
-					'2.0.3': undefined,
-					'3.0.0': undefined,
-					'3.0.1': undefined
-				}
-			});
+		st.plan(3);
+		deprecations('forms', 'object.assign').then(function (messages) {
+			st.deepEqual(compact(messages.forms), compact(formMessages), 'forms messages match');
+			st.deepEqual(Object.keys(messages), ['forms', 'object.assign'], 'messages has two expected keys');
+			st.deepEqual(compact(messages['object.assign']), {
+				'0.1.0': 'Please upgrade to the latest spec-compliant version',
+				'0.1.1': 'Please upgrade to the latest spec-compliant version',
+				'0.2.0': 'Please upgrade to the latest spec-compliant version',
+				'0.2.1': 'Please upgrade to the latest spec-compliant version',
+				'0.3.1': 'Object.assign should accept any value except null and undefined, and should coerce to an object',
+				'0.4.0': 'Please update to the latest version',
+				'0.4.2': 'v0.4.3 doesn\'t modify function arguments, since it deoptimizes v8'
+			}, 'object.assign messages match');
 		}).done();
 	});
 });

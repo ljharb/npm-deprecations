23eb69ea46774a2e938c320226cd7aeac78b3409
Jordan Harband <ljharb@gmail.com>
2016-09-30 14:06:11 -0700

    Revert "[Refactor] only make one `npm info` call, instead of two."
    This reverts commit f20491895a2e490a17fe7b5332cd238299cf0717.

--- messages.js
+++ messages.js
@@ -3,25 +3,28 @@
 var Promise = require('./promise');
 var exec = require('child_process').exec;
 
-module.exports = function getDeprecationMessages(name) {
-	return new Promise(function (resolve, reject) {
-		exec('npm info ' + name + '@* deprecated --no-color --no-spin --loglevel=info', function (err, textMsgs) {
-			if (err) { return reject(err); }
-			return resolve(textMsgs);
-		});
-	}).then(function (textMsgs) {
-		return textMsgs.split('\n').map(function (line) {
-			var unquoted = line.slice(name.length + 1);
-			var version = unquoted.slice(0, unquoted.indexOf(' '));
-			var quotedMessage = unquoted.slice(unquoted.indexOf(' ') + 1);
-
-			var message = quotedMessage.slice(1, -1).replace(/\\'/g, "'").trim();
-
-			var finalData = {};
-			if (message.length > 0) {
-				finalData[version] = message;
+module.exports = function getDeprecationMessages(name, versions) {
+	var promises = versions.map(function (version) {
+		return new Promise(function (resolve, reject) {
+			exec('npm info ' + name + '@' + version + ' deprecated --json --no-spin --loglevel=info', function (err, jsonMsg) {
+				if (err) { return reject(err); }
+				return resolve(String(jsonMsg).trim());
+			});
+		}).then(function (jsonMsg) {
+			if (typeof jsonMsg !== 'undefined' && jsonMsg !== 'undefined') {
+				return JSON.parse(jsonMsg);
 			}
+			return null;
+		}).then(function (fullMsg) {
+			var msg = fullMsg ? String(fullMsg).trim() : '';
+			var finalMessage;
+			if (msg && msg.length > 0) {
+				finalMessage = msg;
+			}
+			var finalData = {};
+			finalData[version] = finalMessage;
 			return finalData;
 		});
 	});
+	return Promise.all(promises);
 };
